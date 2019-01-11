2b756a06786a21e647635a41ad54f29e27d1f456
nicholastrunkey <nicktrunkey@gmail.com>
2019-01-10 16:22:17 -0800

    Eddited styles and expandability

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     $('.locA').click(function() {
       $('.pinA').css({"border": "1px orange solid"});
-      $('.pinB, .pinC').css({"border": "none"});
+      $('.pinB, .pinC').css({"border": "1px #111111 solid"});
     });
     $('.locB').click(function() {
       $('.pinB').css({"border": "1px orange solid"});
-      $('.pinA, .pinC').css({"border": "none"});
+      $('.pinA, .pinC').css({"border": "1px #111111 solid"});
     });
     $('.locC').click(function() {
       $('.pinC').css({"border": "1px orange solid"});
-      $('.pinA, .pinB').css({"border": "none"});
+      $('.pinA, .pinB').css({"border": "1px #111111 solid"});
     });
     $('.mapArea').click(function() {
-      $('.pinA, .pinB, .pinC').css({"border": "none"});
+      $('.pinA, .pinB, .pinC').css({"border": "1px #111111 solid"});
     });
 });
