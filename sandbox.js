af57664bf90fe323dec0eaf0b31b627dd4d121a5
nicholastrunkey <nicktrunkey@gmail.com>
2019-01-31 12:45:40 -0800

    image fixes and sandbox shapes added

--- sandbox.js
+++ sandbox.js
@@ -14,27 +14,32 @@ $(document).ready(function() {
     $('.mapArea').click(function() {
       $('.pinA, .pinB, .pinC').css({"border": "1px #111111 solid"});
     });
+
     $('.genElement').click(function() {
       $elementCount = $('.makeElement').children().length;
-
       if ($elementCount < 4) {
         $('.makeElement').append("<li>List item!</li>");
       } else {
         alert('You have generated enough elements, stop.');
       }
     })
+
     $('.deleteElement li').click(function(clicked) {
       $(clicked.target).remove();
     })
-    $('.inputButton').on('click', function() {
-      $inputCount = $('.userInput').children().length;
-      $inputContents = $('.input').val();
 
-      if ($inputCount < 4) {
-        $('.userInput').append("<li>" + $inputContents + "</li>");
-        $('.input').val("");
-      } else {
-        alert('You have generated enough elements, stop.');
+    $('.input').keypress(function (e) {
+      if (e.which == 13) {
+        $inputCount = $('.userInput').children().length;
+        $inputContents = $('.input').val();
+
+        if ($inputCount < 4) {
+          $('.userInput').append("<li>" + $inputContents + "</li>");
+          $('.input').val("");
+        } else {
+          alert('You have generated enough elements, stop.');
+        }
+        return false;
       }
-    })
+    });
 });
